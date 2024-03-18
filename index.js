e72660b04869eb2ab756ef34e0b7759cdfea4111
Ali Hager <ahager2013@gmail.com>
2024-03-18 10:02:22 -0400

    data seeded

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/a
 const app = express();
 
 const init = async()=> {
+  console.log('connecting to database');
   await client.connect();
   console.log('connected to database');
+  let SQL=`
+    DROP TABLE IF EXISTS flavor;
+    CREATE TABLE flavor(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    is_favorite BOOLEAN,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+    );
+  `;
+  await client.query(SQL);
+  console.log('tables created');
+
+  SQL=`
+      INSERT INTO flavor(name, is_favorite) VALUES ('mint chocolate chip', TRUE);
+      INSERT INTO flavor(name, is_favorite) VALUES ('chocolate chip cookie dough', FALSE);
+      INSERT INTO flavor(name, is_favorite) VALUES ('butter pecan', FALSE);
+  `;
+  await client.query(SQL);
+  console.log('data seeded');
+
 };
 init();
